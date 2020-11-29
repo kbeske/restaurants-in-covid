ef041981a476829ec8fb94221e5c896f4e801b33
kbeske <beske.k@husky.neu.edu>
2020-11-29 15:04:06 -0500

    more cuisine closed queries and adding results to a table

--- yelp_covid.sql
+++ yelp_covid.sql
@@ -28,6 +28,18 @@ select cuisine, count(cuisine)
 from closed_restaurant
 group by cuisine;
 
+-- any cuisines
+select cuisine, count(cuisine)
+from restaurant
+group by cuisine;
+
+-- create table for number of closed restaurants by cuisine
+create table cuisine_num_closed (
+	cuisine varchar(50) primary key,
+	num_closed int,
+    total int
+);
+
 -- 27 total "american" cuisine restaurants closed 
 select count(*)
 from closed_restaurant
@@ -40,6 +52,15 @@ where cuisine like '%, american%' or cuisine like 'american%'; -- 544
 select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%, american%' or cuisine like 'american%'; -- 4.96
+
+insert into cuisine_num_closed values ('American', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%, american%' or cuisine like 'american%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%, american%' or cuisine like 'american%'));
+    
 
 -- italian cuisine
 select count(*)
@@ -54,6 +75,15 @@ select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%, italian%' or cuisine like 'italian%'; -- 3.04
 
+insert into cuisine_num_closed values ('Italian', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%, italian%' or cuisine like 'italian%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%, italian%' or cuisine like 'italian%'));
+
+
 -- breakfast
 select count(*)
 from closed_restaurant
@@ -67,18 +97,71 @@ select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%, breakfast%' or cuisine like 'breakfast%'; -- 4.65
 
+insert into cuisine_num_closed values ('Breakfast', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%, breakfast%' or cuisine like 'breakfast%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%, breakfast%' or cuisine like 'breakfast%'));
+
 -- bars (any bars, not just sports bars or dive bars)
 select count(*)
 from closed_restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 29
+where cuisine like '%cocktail bars%' or
+cuisine like '%wine bars%' or
+cuisine like '%sports bars%' or
+cuisine like '%dive bars%' or
+cuisine like '%tiki bars%' or
+cuisine like '%beer bar%' or
+cuisine like '%pubs%' or
+cuisine like '%bars%'; -- 39
 
 select count(*)
 from restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 750
+where cuisine like '%cocktail bars%' or
+cuisine like '%wine bars%' or
+cuisine like '%sports bars%' or
+cuisine like '%dive bars%' or
+cuisine like '%tiki bars%' or
+cuisine like '%beer bar%' or
+cuisine like '%pubs%' or
+cuisine like '%bars%'; -- 838
 
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'; -- 3.87
+insert into cuisine_num_closed values ('Bars', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%, bars%' or cuisine like '% bars%' or cuisine like 'bars%'));
+
+update cuisine_num_closed
+set num_closed = (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%cocktail bars%' or
+	cuisine like '%wine bars%' or
+	cuisine like '%sports bars%' or
+	cuisine like '%dive bars%' or
+	cuisine like '%tiki bars%' or
+	cuisine like '%beer bar%' or
+	cuisine like '%pubs%' or
+	cuisine like '%bars%'),
+total = (
+	select count(*)
+	from restaurant
+	where cuisine like '%cocktail bars%' or
+	cuisine like '%wine bars%' or
+	cuisine like '%sports bars%' or
+	cuisine like '%dive bars%' or
+	cuisine like '%tiki bars%' or
+	cuisine like '%beer bar%' or
+	cuisine like '%pubs%' or
+	cuisine like '%bars%')
+where cuisine = "Bars";
+
+select * from cuisine_num_closed;
 
 -- mexican cuisine
 select count(*)
@@ -93,6 +176,14 @@ select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%, mexican%' or cuisine like 'mexican%'; -- .58
 
+insert into cuisine_num_closed values ('Mexican', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%, mexican%' or cuisine like 'mexican%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%, mexican%' or cuisine like 'mexican%'));
+
 -- food trucks
 select count(*)
 from closed_restaurant
@@ -105,6 +196,14 @@ where cuisine like '%food trucks%'; -- 62
 select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%food trucks%'; -- 0
+
+insert into cuisine_num_closed values ('Food Trucks', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%food trucks%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%food trucks%'));
 
 -- coffee and tea
 select count(*)
@@ -119,18 +218,171 @@ select round((sum(is_closed) / count(*)) * 100, 2)
 from restaurant
 where cuisine like '%coffee & tea%'; -- 1.27
 
+insert into cuisine_num_closed values ('Coffee & Tea', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%coffee & tea%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%coffee & tea%'));
+    
+select * from cuisine_num_closed;
+
 -- japanese
 select count(*)
 from closed_restaurant
-where cuisine like '%japanese%'; -- 4
+where cuisine like '%japanese%' or
+cuisine like '%ramen%' or
+cuisine like '%sushi%'; -- 4
 
 select count(*)
 from restaurant
-where cuisine like '%japanese%'; -- 108
+where cuisine like '%japanese%' or
+cuisine like '%ramen%' or
+cuisine like '%sushi%'; -- 182
 
-select round((sum(is_closed) / count(*)) * 100, 2)
-from restaurant
-where cuisine like '%japanese%'; -- 3.70
+insert into cuisine_num_closed values ('Japanese', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%japanese%' or
+	cuisine like '%ramen%' or
+	cuisine like '%sushi%'), (
+    select count(*)
+	from restaurant
+    where cuisine like '%japanese%' or
+	cuisine like '%ramen%' or
+	cuisine like '%sushi%'));
+
+-- more cuisines
+
+insert into cuisine_num_closed values ('French', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%french%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%french%'));
+    
+insert into cuisine_num_closed values ('Ice Cream', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%ice cream%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%ice cream%'));
+    
+insert into cuisine_num_closed values ('Korean', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%korean%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%korean%'));
+    
+insert into cuisine_num_closed values ('Thai', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%thai%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%thai%'));
+    
+insert into cuisine_num_closed values ('Vietnamese', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%vietnamese%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%vietnamese%'));
+    
+insert into cuisine_num_closed values ('Spanish', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%spanish%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%spanish%'));
+
+select count(*)
+	from restaurant
+	where cuisine like '%Mediterranean%';
+
+insert into cuisine_num_closed values ('Mediterranean', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%mediterranean%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%mediterranean%'));
+    
+insert into cuisine_num_closed values ('Sandwiches', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%sandwiches%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%sandwiches%'));
+    
+insert into cuisine_num_closed values ('Tapas', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%tapas%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%tapas%'));
+    
+insert into cuisine_num_closed values ('Irish', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%irish%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%irish%'));
+    
+insert into cuisine_num_closed values ('Middle Eastern', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%middle eastern%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%middle eastern%'));
+    
+insert into cuisine_num_closed values ('Chinese', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%chinese%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%chinese%'));
+    
+insert into cuisine_num_closed values ('Latin American', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%latin american%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%latin american%'));
+    
+insert into cuisine_num_closed values ('Juice Bar', (
+	select count(*)
+	from closed_restaurant
+	where cuisine like '%juice bar%'), (
+    select count(*)
+	from restaurant
+	where cuisine like '%juice bar%'));
+    
+select * from restaurant where cuisine like '%ramen%';
+select * from closed_restaurant where cuisine like '%sushi bar%';
+    
+select * from cuisine_num_closed;
+
+-- create view for percentages
+create or replace view cuisine_pct_closed as
+select cuisine, round((num_closed/total * 100), 2) as pct_closed
+from cuisine_num_closed;
+
+select * from cuisine_pct_closed;
+select * from cuisine_pct_closed order by pct_closed desc;
 
 -- How many closed restaurants are in the same zip codes as colleges/universities 
 
